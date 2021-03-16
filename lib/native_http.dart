6a20c8be1f493d64ec2593a8ab331ea4a37fe50e
wangrui <wangrui@oecore.com>
2021-03-16 14:36:17 +0800

    1. support null safety.

--- lib/native_http.dart
+++ lib/native_http.dart
@@ -16,24 +16,29 @@ Future<NativeResponse> get(
 Future<NativeResponse> post(
   String url, {
   Map<String, dynamic> headers = const {},
-  Uint8List body,
+  Uint8List? body,
 }) {
-  body = body ?? Uint8List(0);
   return request(url: url, method: "POST", headers: headers, body: body);
 }
 
-Future<NativeResponse> request(
-    {String url,
-    String method,
-    Map<String, dynamic> headers,
-    Uint8List body}) async {
-  Map<String, dynamic> response =
-      await _channel.invokeMapMethod<String, dynamic>("native_http/request", {
+Future<NativeResponse> request({
+  required String url,
+  required String method,
+  required Map<String, dynamic> headers,
+  Uint8List? body
+}) async {
+  var req = {
     "url": url,
     "method": method,
     "headers": headers,
-    "body": body,
-  });
+    "body": body ?? Uint8List(0),
+  };
+
+  Map<String, dynamic>? response = await _channel.invokeMapMethod<String, dynamic>("native_http/request", req);
+
+  if (response == null)
+    throw Exception("response is null for $req.");
+
   return NativeResponse._fromMap(response);
 }
 
@@ -41,11 +46,11 @@ class NativeResponse {
   int code;
   Uint8List body;
 
+  NativeResponse({ required this.code, required this.body });
+
   dynamic getJson() => json.decode(utf8.decode(body));
 
   static NativeResponse _fromMap(Map<String, dynamic> response) {
-    return NativeResponse()
-      ..code = response["code"]
-      ..body = response["body"];
+    return NativeResponse(code: response["code"], body: response["body"]);
   }
 }
